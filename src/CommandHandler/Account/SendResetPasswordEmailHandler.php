a9267af66935e6227a0bdf3c8fc0703cfe312c0a
Tomasz Grochowski <tomasz.grochowski@bitbag.pl>
2021-09-09 12:49:07 +0200

    Reset password mail send

---
+++ src/CommandHandler/Account/SendResetPasswordEmailHandler.php
@@ -0,0 +1,70 @@
+<?php
+
+/*
+ * This file was created by developers working at BitBag
+ * Do you need more information about us and what we do? Visit our https://bitbag.io website!
+ * We are hiring developers from all over the world. Join us and start your new, exciting adventure and become part of us: https://bitbag.io/career
+*/
+
+declare(strict_types=1);
+
+namespace BitBag\SyliusGraphqlPlugin\CommandHandler\Account;
+
+use BitBag\SyliusGraphqlPlugin\Command\Account\SendResetPasswordEmail;
+use Sylius\Bundle\CoreBundle\Mailer\Emails;
+use Sylius\Component\Channel\Context\ChannelContextInterface;
+use Sylius\Component\Core\Model\CustomerInterface;
+use Sylius\Component\Core\Model\ShopUserInterface;
+use Sylius\Component\Mailer\Sender\SenderInterface;
+use Sylius\Component\User\Repository\UserRepositoryInterface;
+use Sylius\Component\User\Security\Generator\GeneratorInterface;
+use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+use Webmozart\Assert\Assert;
+
+/** @experimental */
+final class SendResetPasswordEmailHandler implements MessageHandlerInterface
+{
+    private SenderInterface $emailSender;
+
+    private ChannelContextInterface $channelContext;
+
+    private UserRepositoryInterface $userRepository;
+
+    private GeneratorInterface $generator;
+
+    public function __construct(
+        SenderInterface $emailSender,
+        ChannelContextInterface $channelContext,
+        UserRepositoryInterface $userRepository,
+        GeneratorInterface $generator
+    ) {
+        $this->emailSender = $emailSender;
+        $this->channelContext = $channelContext;
+        $this->userRepository = $userRepository;
+        $this->generator = $generator;
+    }
+
+    public function __invoke(SendResetPasswordEmail $command): CustomerInterface
+    {
+        /** @var ShopUserInterface|null $user */
+        $user = $this->userRepository->findOneByEmail($command->email);
+        Assert::notNull($user, 'User with following email could not be found.');
+
+        $channel = $this->channelContext->getChannel();
+
+        $user->setPasswordResetToken($this->generator->generate());
+        $user->setPasswordRequestedAt(new \DateTime());
+
+        $this->emailSender->send(
+            Emails::PASSWORD_RESET,
+            [$command->email],
+            [
+                'user' => $user,
+                'localeCode' => $command->localeCode,
+                'channel' => $channel,
+            ]
+        );
+
+        return $user->getCustomer();
+    }
+}
