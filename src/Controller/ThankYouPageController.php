dd43ff2f141532750a5de0a60422993fa95332c9
Marcin Kukliński <senghe@gmail.com>
2022-11-24 23:10:32 +0100

    Add redirecting PayPal back to the VSF2 thank you page

---
+++ src/Controller/ThankYouPageController.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file was created by developers working at BitBag
+ * Do you need more information about us and what we do? Visit our https://bitbag.io website!
+ * We are hiring developers from all over the world. Join us and start your new, exciting adventure and become part of us: https://bitbag.io/career
+*/
+
+declare(strict_types=1);
+
+namespace BitBag\SyliusVueStorefront2Plugin\Controller;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RouterInterface;
+
+final class ThankYouPageController
+{
+    private ?string $vsf2Host;
+
+    private RouterInterface $router;
+
+    public function __construct(
+        ?string $vsf2Host,
+        RouterInterface $router,
+    ) {
+        $this->vsf2Host = $vsf2Host;
+        $this->router = $router;
+    }
+
+    public function redirectAction(Request $request): Response
+    {
+        if ($this->vsf2Host !== null && strlen($this->vsf2Host) > 0) {
+            $orderNumber = $request->attributes->get('orderNumber');
+            if ($orderNumber === null) {
+                return new RedirectResponse(
+                    $this->router->generate('sylius_shop_homepage'),
+                );
+            }
+
+            return new RedirectResponse(sprintf('%s/checkout/thank-you?order=%s', $this->vsf2Host, (string)$orderNumber));
+        }
+
+        return new RedirectResponse(
+            $this->router->generate('sylius_shop_homepage'),
+        );
+    }
+}
