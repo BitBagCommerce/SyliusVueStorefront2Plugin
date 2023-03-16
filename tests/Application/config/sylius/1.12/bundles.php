dcf393672e2781dea9bf3b84b6a72d22ed9a16a8
Rafal Malenta <rafal.malenta.extern@foodspring.com>
2023-03-16 13:55:16 +0100

    First push

---
+++ tests/Application/config/sylius/1.12/bundles.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    League\FlysystemBundle\FlysystemBundle::class => ['all', true]
+//    SyliusLabs\Polyfill\Symfony\Security\Bundle\SyliusLabsPolyfillSymfonySecurityBundle::class => ['all' => true],
+];
