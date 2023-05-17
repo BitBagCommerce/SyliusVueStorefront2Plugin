3ccd91cca4ca48963369aaf81faf8e0364488d62
kloz <klaudia.lozowska@bitbag.com>
2023-05-17 16:26:09 +0200

    [VSF2-177] Taxon decorated repository - methods fix

--- src/Doctrine/Repository/TaxonRepository.php
+++ src/Doctrine/Repository/TaxonRepository.php
@@ -39,131 +39,42 @@ final class TaxonRepository extends NestedTreeRepository implements TaxonReposit
 
     public function findChildren(string $parentCode, ?string $locale = null): array
     {
-        return $this->createTranslationBasedQueryBuilder($locale)
-            ->addSelect('child')
-            ->innerJoin('o.parent', 'parent')
-            ->leftJoin('o.children', 'child')
-            ->andWhere('parent.code = :parentCode')
-            ->addOrderBy('o.position')
-            ->setParameter('parentCode', $parentCode)
-            ->getQuery()
-            ->getResult()
-            ;
+        return $this->decoratedRepository->findChildren($parentCode, $locale);
     }
 
     public function findChildrenByChannelMenuTaxon(?TaxonInterface $menuTaxon = null, ?string $locale = null): array
     {
-        $hydrationQuery = $this->createTranslationBasedQueryBuilder($locale)
-            ->addSelect('o')
-            ->addSelect('oc')
-            ->leftJoin('o.children', 'oc')
-        ;
-
-        if (null !== $menuTaxon) {
-            $hydrationQuery
-                ->andWhere('o.root = :root')
-                ->setParameter('root', $menuTaxon)
-            ;
-        }
-
-        $hydrationQuery->getQuery()->getResult();
-
-        return $this->createTranslationBasedQueryBuilder($locale)
-            ->addSelect('child')
-            ->innerJoin('o.parent', 'parent')
-            ->leftJoin('o.children', 'child')
-            ->andWhere('o.enabled = :enabled')
-            ->andWhere('parent.code = :parentCode')
-            ->addOrderBy('o.position')
-            ->setParameter('parentCode', ($menuTaxon !== null) ? $menuTaxon->getCode() : 'category')
-            ->setParameter('enabled', true)
-            ->getQuery()
-            ->getResult()
-            ;
+        return $this->decoratedRepository->findChildrenByChannelMenuTaxon($menuTaxon, $locale);
     }
 
     public function findRootNodes(): array
     {
-        return $this->createQueryBuilder('o')
-            ->andWhere('o.parent IS NULL')
-            ->addOrderBy('o.position')
-            ->getQuery()
-            ->getResult()
-            ;
+        return $this->decoratedRepository->findRootNodes();
     }
 
     public function findHydratedRootNodes(): array
     {
-        $this->createQueryBuilder('o')
-            ->select(['o', 'oc', 'ot'])
-            ->leftJoin('o.children', 'oc')
-            ->leftJoin('o.translations', 'ot')
-            ->getQuery()
-            ->getResult()
-        ;
-
-        return $this->findRootNodes();
+        return $this->decoratedRepository->findHydratedRootNodes();
     }
 
     public function findOneBySlug(string $slug, string $locale): ?TaxonInterface
     {
-        return $this->createQueryBuilder('o')
-            ->addSelect('translation')
-            ->innerJoin('o.translations', 'translation')
-            ->andWhere('o.enabled = :enabled')
-            ->andWhere('translation.slug = :slug')
-            ->andWhere('translation.locale = :locale')
-            ->setParameter('slug', $slug)
-            ->setParameter('locale', $locale)
-            ->setParameter('enabled', true)
-            ->getQuery()
-            ->getOneOrNullResult()
-            ;
+        return $this->decoratedRepository->findOneBySlug($slug, $locale);
     }
 
     public function findByName(string $name, string $locale): array
     {
-        return $this->createQueryBuilder('o')
-            ->addSelect('translation')
-            ->innerJoin('o.translations', 'translation')
-            ->andWhere('translation.name = :name')
-            ->andWhere('translation.locale = :locale')
-            ->setParameter('name', $name)
-            ->setParameter('locale', $locale)
-            ->getQuery()
-            ->getResult()
-            ;
+        return $this->decoratedRepository->findByName($name, $locale);
     }
 
     public function findByNamePart(string $phrase, ?string $locale = null, ?int $limit = null): array
     {
-        /** @var TaxonInterface[] $results */
-        $results = $this->createTranslationBasedQueryBuilder($locale)
-            ->andWhere('translation.name LIKE :name')
-            ->setParameter('name', '%' . $phrase . '%')
-            ->setMaxResults($limit)
-            ->getQuery()
-            ->getResult()
-        ;
-
-        foreach ($results as $result) {
-            $result->setFallbackLocale(array_key_first($result->getTranslations()->toArray()));
-        }
-
-        return $results;
+        return $this->decoratedRepository->findByNamePart($phrase, $locale, $limit);
     }
 
     public function createListQueryBuilder(): QueryBuilder
     {
-        return $this->createQueryBuilder('o')->leftJoin('o.translations', 'translation');
-    }
-
-    private function createTranslationBasedQueryBuilder(?string $locale): QueryBuilder
-    {
-        $alias = 'o';
-        $queryBuilder = $this->createQueryBuilder($alias);
-
-        return $this->addTranslations($queryBuilder, $alias, $locale);
+        return $this->decoratedRepository->createQueryBuilder('o');
     }
 
     private function addTranslations(QueryBuilder $queryBuilder, string $alias, ?string $locale): QueryBuilder
