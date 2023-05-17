5826a6d3086b005fe8f54be7352809e7b5aed9ec
kloz <klaudia.lozowska@bitbag.com>
2023-05-17 14:46:50 +0200

    Revert "[VSF2-177] Taxon serialization using DTO"
    This reverts commit 287f9791a7ca63993af8075c20e5fbd7627589d4.

--- src/DataProvider/TaxonCollectionDataProvider.php
+++ src/DataProvider/TaxonCollectionDataProvider.php
@@ -18,8 +18,6 @@ use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
 use ApiPlatform\Core\DataProvider\CollectionDataProviderInterface;
 use ApiPlatform\Core\DataProvider\RestrictedDataProviderInterface;
 use BitBag\SyliusVueStorefront2Plugin\Doctrine\Repository\TaxonRepositoryInterface;
-use BitBag\SyliusVueStorefront2Plugin\DTO\Taxon\TaxonDto;
-use BitBag\SyliusVueStorefront2Plugin\DTO\Taxon\TaxonParentDto;
 use Sylius\Bundle\ApiBundle\Context\UserContextInterface;
 use Sylius\Bundle\ApiBundle\Serializer\ContextKeys;
 use Sylius\Component\Core\Model\ChannelInterface;
@@ -68,7 +66,7 @@ final class TaxonCollectionDataProvider implements CollectionDataProviderInterfa
         $channelMenuTaxon = $channelContext->getMenuTaxon();
 
         $user = $this->userContext->getUser();
-        if ($this->hasAccessToAllTaxa($user)) {
+        if ($this->isUserAllowedToGetAllTaxa($user)) {
             return $this->taxonRepository->findAll();
         }
 
@@ -85,49 +83,21 @@ final class TaxonCollectionDataProvider implements CollectionDataProviderInterfa
             }
 
             if ($extension instanceof QueryResultCollectionExtensionInterface && $extension->supportsResult($resourceClass, $operationName)) {
-                return $this->parseResult($extension->getResult($queryBuilder));
+                return $extension->getResult($queryBuilder);
             }
         }
 
-        return $this->parseResult(
-            $this->paginationExtension->getResult(
-                $queryBuilder,
-                $resourceClass,
-                $operationName,
-                $context,
-            )
+        return $this->paginationExtension->getResult(
+            $queryBuilder,
+            $resourceClass,
+            $operationName,
+            $context,
         );
     }
 
-    private function hasAccessToAllTaxa(?UserInterface $user): bool
+    private function isUserAllowedToGetAllTaxa(?UserInterface $user): bool
     {
         /** @psalm-suppress DeprecatedClass */
         return $user !== null && in_array('ROLE_API_ACCESS', $user->getRoles(), true);
-    }
-
-    private function parseResult(iterable $result): array
-    {
-        $data = [];
-        foreach ($result as $taxon) {
-            $data[] = $this->parseTaxon($taxon);
-        }
-
-        return $data;
-    }
-
-    private function parseTaxon(TaxonInterface $taxon): TaxonDto
-    {
-        return new TaxonDto(
-            $taxon->getId(),
-            $taxon->getName(),
-            $taxon->getCode(),
-            $taxon->getPosition(),
-            $taxon->getSlug(),
-            $taxon->getDescription(),
-            $taxon->isEnabled(),
-            $taxon->getLevel(),
-            $taxon->getTranslations(),
-            $taxon->getParent() ? new TaxonParentDto($taxon->getParent()) : null,
-        );
     }
 }
