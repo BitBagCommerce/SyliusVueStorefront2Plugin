b60442753134e91dcf0444872c211167eef87efe
kloz <klaudia.lozowska@bitbag.com>
2023-06-16 13:42:03 +0200

    [VSF2-159] Phpspec tests

---
+++ spec/DataGenerator/Generator/EntityGenerator/TaxonGeneratorSpec.php
@@ -0,0 +1,95 @@
+<?php
+/*
+ * This file was created by developers working at BitBag
+ * Do you need more information about us and what we do? Visit our https://bitbag.io website!
+ * We are hiring developers from all over the world. Join us and start your new, exciting adventure and become part of us: https://bitbag.io/career
+ */
+
+declare(strict_types=1);
+
+namespace spec\BitBag\SyliusVueStorefront2Plugin\DataGenerator\Generator\EntityGenerator;
+
+use BitBag\SyliusVueStorefront2Plugin\DataGenerator\ContextModel\EntityContext\EntityContextInterface;
+use BitBag\SyliusVueStorefront2Plugin\DataGenerator\ContextModel\EntityContext\TaxonContextInterface;
+use BitBag\SyliusVueStorefront2Plugin\DataGenerator\Doctrine\Repository\TaxonRepositoryInterface;
+use BitBag\SyliusVueStorefront2Plugin\DataGenerator\Exception\InvalidContextException;
+use BitBag\SyliusVueStorefront2Plugin\DataGenerator\Factory\TaxonFactoryInterface;
+use BitBag\SyliusVueStorefront2Plugin\DataGenerator\Generator\EntityGenerator\TaxonGenerator;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Sylius\Component\Core\Model\TaxonInterface;
+use Sylius\Component\Taxonomy\Model\TaxonTranslationInterface;
+
+class TaxonGeneratorSpec extends ObjectBehavior
+{
+    public function let(
+        TaxonFactoryInterface $taxonFactory,
+        TaxonRepositoryInterface $taxonRepository,
+    ): void {
+        $this->beConstructedWith($taxonFactory, $taxonRepository);
+    }
+
+    public function it_is_initializable(): void
+    {
+        $this->shouldHaveType(TaxonGenerator::class);
+    }
+
+    public function it_generates_with_main_taxon_as_parent(
+        TaxonFactoryInterface $taxonFactory,
+        TaxonRepositoryInterface $taxonRepository,
+        TaxonContextInterface $context,
+        TaxonInterface $parent,
+        TaxonInterface $taxon,
+    ): void {
+        $maxTaxonLevel = 100;
+        $maxChildrenPerTaxonLevel = 100;
+
+        $context->getMaxTaxonLevel()->willReturn($maxTaxonLevel);
+        $context->getMaxChildrenPerTaxonLevel()->willReturn($maxChildrenPerTaxonLevel);
+        $taxonRepository->findEligibleParents($maxTaxonLevel, $maxChildrenPerTaxonLevel)
+            ->willReturn([]);
+        $taxonRepository->getMainTaxon()->willReturn($parent->getWrappedObject());
+
+        $taxonFactory
+            ->create(
+                Argument::type('string'),
+                $parent->getWrappedObject(),
+                Argument::type(TaxonTranslationInterface::class)
+            )
+            ->willReturn($taxon->getWrappedObject());
+
+        $this->generate($context);
+    }
+
+    public function it_generates_with_random_parent(
+        TaxonFactoryInterface $taxonFactory,
+        TaxonRepositoryInterface $taxonRepository,
+        TaxonContextInterface $context,
+        TaxonInterface $parent,
+        TaxonInterface $taxon,
+    ): void {
+        $maxTaxonLevel = 100;
+        $maxChildrenPerTaxonLevel = 100;
+
+        $context->getMaxTaxonLevel()->willReturn($maxTaxonLevel);
+        $context->getMaxChildrenPerTaxonLevel()->willReturn($maxChildrenPerTaxonLevel);
+        $taxonRepository->findEligibleParents($maxTaxonLevel, $maxChildrenPerTaxonLevel)
+            ->willReturn([$parent->getWrappedObject()]);
+
+        $taxonFactory
+            ->create(
+                Argument::type('string'),
+                $parent->getWrappedObject(),
+                Argument::type(TaxonTranslationInterface::class)
+            )
+            ->willReturn($taxon->getWrappedObject());
+
+        $this->generate($context);
+    }
+
+    public function it_throws_exception_on_invalid_context(EntityContextInterface $context): void
+    {
+        $this->shouldThrow(InvalidContextException::class)
+            ->during('generate', [$context]);
+    }
+}
--- spec/DataGenerator/Generator/EntityGenerator/WishlistGeneratorSpec.php
+++ spec/DataGenerator/Generator/EntityGenerator/WishlistGeneratorSpec.php
@@ -11,11 +11,15 @@ namespace spec\BitBag\SyliusVueStorefront2Plugin\DataGenerator\Generator\EntityG
 
 use BitBag\SyliusVueStorefront2Plugin\DataGenerator\ContextModel\EntityContext\EntityContextInterface;
 use BitBag\SyliusVueStorefront2Plugin\DataGenerator\ContextModel\EntityContext\ProductContextInterface;
+use BitBag\SyliusVueStorefront2Plugin\DataGenerator\ContextModel\EntityContext\WishlistContextInterface;
 use BitBag\SyliusVueStorefront2Plugin\DataGenerator\Exception\InvalidContextException;
 use BitBag\SyliusVueStorefront2Plugin\DataGenerator\Factory\ChannelPricingFactoryInterface;
 use BitBag\SyliusVueStorefront2Plugin\DataGenerator\Factory\ProductFactoryInterface;
 use BitBag\SyliusVueStorefront2Plugin\DataGenerator\Factory\ProductVariantFactoryInterface;
+use BitBag\SyliusVueStorefront2Plugin\DataGenerator\Factory\WishlistFactoryInterface;
 use BitBag\SyliusVueStorefront2Plugin\DataGenerator\Generator\EntityGenerator\ProductGenerator;
+use BitBag\SyliusVueStorefront2Plugin\DataGenerator\Generator\EntityGenerator\WishlistGenerator;
+use BitBag\SyliusWishlistPlugin\Entity\WishlistInterface;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 use Sylius\Component\Core\Model\ChannelInterface;
@@ -23,59 +27,33 @@ use Sylius\Component\Core\Model\ChannelPricingInterface;
 use Sylius\Component\Core\Model\ProductInterface;
 use Sylius\Component\Core\Model\ProductVariantInterface;
 
-class ProductGeneratorSpec extends ObjectBehavior
+class WishlistGeneratorSpec extends ObjectBehavior
 {
-    public function let(
-        ProductFactoryInterface $productFactory,
-        ProductVariantFactoryInterface $productVariantFactory,
-        ChannelPricingFactoryInterface $channelPricingFactory,
-    ): void {
-        $this->beConstructedWith($productFactory, $productVariantFactory, $channelPricingFactory);
+    public function let(WishlistFactoryInterface $wishlistFactory): void
+    {
+        $this->beConstructedWith($wishlistFactory);
     }
 
     public function it_is_initializable(): void
     {
-        $this->shouldHaveType(ProductGenerator::class);
+        $this->shouldHaveType(WishlistGenerator::class);
     }
 
     public function it_generates(
-        ProductFactoryInterface $productFactory,
-        ProductVariantFactoryInterface $productVariantFactory,
-        ChannelPricingFactoryInterface $channelPricingFactory,
+        WishlistFactoryInterface $wishlistFactory,
+        WishlistContextInterface $context,
         ChannelInterface $channel,
-        ChannelPricingInterface $channelPricing,
-        ProductVariantInterface $productVariant,
-        ProductContextInterface $context,
-        ProductInterface $product,
+        WishlistInterface $wishlist,
     ): void {
-        $context->getChannel()->willReturn($channel);
-        $channel->getCode()->willReturn(Argument::type('string'));
-
-        $channelPricingFactory
-            ->create(Argument::type('integer'), $channel)
-            ->willReturn($channelPricing);
-
-        $productVariantFactory
-            ->create(
-                Argument::type('string'),
-                Argument::type('string'),
-                $channelPricing->getWrappedObject()
-            )
-            ->willReturn($productVariant);
-
         $context->getChannel()->willReturn($channel->getWrappedObject());
 
-        $productFactory
+        $wishlistFactory
             ->create(
                 Argument::type('string'),
                 Argument::type('string'),
-                Argument::type('string'),
-                Argument::type('string'),
-                $productVariant->getWrappedObject(),
                 $channel->getWrappedObject(),
-                Argument::type(\DateTime::class),
             )
-            ->willReturn($product);
+            ->willReturn($wishlist);
 
         $this->generate($context);
     }
